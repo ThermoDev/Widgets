0e5094aba7e09987cdf6ed4db3507b47633bf7a1
Michael Pegios <michael.pegios@hotmail.com>
2022-02-01 11:13:16 +1100

    onTitleClick Helper Function
    - Created the onTitleClick Helper Function in the Functional component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => {
+  const onTitleClick = (index) => {
+    console.log('Title clicked: ', index);
+  };
+
+  const renderedItems = items.map((item, index) => {
     return (
       <Fragment key={item.title}>
-        <div className="title active">
+        <div className="title active" onClick={() => onTitleClick(index)}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
