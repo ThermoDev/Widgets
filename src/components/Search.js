bd709ea6e9df48d82ede66b5d26316907087c7c4
Michael Pegios <michael.pegios@hotmail.com>
2022-02-04 09:28:12 +1100

    Setting Search Results

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,10 +5,13 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  console.log(results)
 
   useEffect(() => {
     const search = async () => {
-      await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
@@ -17,9 +20,12 @@ const Search = () => {
           srsearch: term,
         },
       });
+      setResults(data.query.search);
     };
 
-    search();
+    if (term){
+      search();
+    }
   }, [term]);
 
   // useEffect second argument
