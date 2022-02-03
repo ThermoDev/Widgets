5d6826ebe226f8301ee3a51c724e57aab68d88d8
Michael Pegios <michael.pegios@hotmail.com>
2022-02-03 16:56:10 +1100

    Making Wikipedia Searches
    - Creating wikipedia searches using axios

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 // https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=programming
 
@@ -6,8 +7,25 @@ const Search = () => {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    console.log('hi!');
+    const search = async () => {
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+    };
+
+    search();
   }, [term]);
+
+  // useEffect second argument
+  // ..nothing..  : Run at initial render -> Run after every rerender
+  // []           : Run at initial render
+  // [date]       : Run at initial render -> Run after every rerender if data has changed since last rerender
 
   return (
     <div>
