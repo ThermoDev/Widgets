426d5c1ebacef97f5a57dbd15451ff1311b6c710
Michael Pegios <michael.pegios@hotmail.com>
2022-02-04 15:36:07 +1100

    Created Toggle Dropdown and Fixed Event Listeners
    - Created the Toggle Dropdown Button
    - Created 'click' event listener
    - Check if ref element target the target element, this ensures that setOpen(false) is only called from the body

--- src/App.js
+++ src/App.js
@@ -35,16 +35,23 @@ const options = [
 
 const App = () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
 
   return (
     <div>
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,30 @@
-import React, { useState } from 'react';
+import React, { Fragment, useEffect, useRef, useState } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // Check if ref element contains the event target element
+      if (ref.current.contains(event.target)) {
+        return;
+      }
+
+      setOpen(false);
+    };
+
+    // addEventListeners called first,
+    // Then, React Event Listeners are bubbled up the DOM
+    document.body.addEventListener('click', onBodyClick, { capture: true });
+
+    // Call cleanup function, remove event listener
+    return () => {
+      document.body.removeEventListener('click', onBodyClick, {
+        capture: true,
+      });
+    };
+  }, []);
 
   const renderedOptions = options.map((option) => {
     if (option.value === selected.value) {
@@ -18,22 +41,28 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
       </div>
     );
   });
+
   return (
-    <div className="ui form">
-      <div className="field">
-        <label className="label">Select a Color</label>
-        <div
-          onClick={() => setOpen(!open)}
-          className={`ui selection dropdown ${open ? 'visible active' : ''}`}
-        >
-          <i className="dropdown icon"></i>
-          <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition' : ''}`}>
-            {renderedOptions}
+    <Fragment>
+      <div ref={ref} className="ui form">
+        <div className="field">
+          <label className="label">Select a Color</label>
+          <div
+            onClick={() => setOpen(!open)}
+            className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+          >
+            <i className="dropdown icon"></i>
+            <div className="text">{selected.label}</div>
+            <div className={`menu ${open ? 'visible transition' : ''}`}>
+              {renderedOptions}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div style={{ color: `${selected.value}` }}>
+        This text is: {`${selected.value}`}
+      </div>
+    </Fragment>
   );
 };
 
