7f998b414ce54b6a32205fe2f2266646a4a08682
Michael Pegios <michael.pegios@hotmail.com>
2022-02-10 14:53:16 +1100

    Display Translated Text
    - Made axios request async/awaitable
    - Created translated prop
    - Return result of translate response

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Convert = ({ language, text }) => {
+  const [translated, setTranslated] = useState('');
   useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-        },
-      }
-    );
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: text,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+    doTranslation();
   }, [language, text]);
 
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="ui header">{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
