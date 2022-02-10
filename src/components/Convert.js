afeab4af2927f0d9a7875baa11a16ec5cd3e0da2
Michael Pegios <michael.pegios@hotmail.com>
2022-02-10 15:17:32 +1100

    Debouncing Text
    - Added DebounceText term to Convert Component

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -3,6 +3,18 @@ import axios from 'axios';
 
 const Convert = ({ language, text }) => {
   const [translated, setTranslated] = useState('');
+  const [debouncedText, setDebouncedText] = useState(text);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
   useEffect(() => {
     const doTranslation = async () => {
       const { data } = await axios.post(
@@ -10,7 +22,7 @@ const Convert = ({ language, text }) => {
         {},
         {
           params: {
-            q: text,
+            q: debouncedText,
             target: language.value,
             key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
           },
@@ -19,7 +31,7 @@ const Convert = ({ language, text }) => {
       setTranslated(data.data.translations[0].translatedText);
     };
     doTranslation();
-  }, [language, text]);
+  }, [language, debouncedText]);
 
   return (
     <div>
