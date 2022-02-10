1b57df2cf90c031913b97746997343b058c8aae8
Michael Pegios <michael.pegios@hotmail.com>
2022-02-10 16:50:48 +1100

    Building Navigation Links
    - Created Header Component with navigation links
    - Added to App

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
   {
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
