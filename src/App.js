14e2d84a739f1126ca168b9b66948a4ba963f4aa
Michael Pegios <michael.pegios@hotmail.com>
2022-02-10 18:08:40 +1100

    Optimizing Navigation
    - Created Custom Link Component
    - Detected Navigation by adding state currentPath props Route
    - Changed <a> to <Link>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
